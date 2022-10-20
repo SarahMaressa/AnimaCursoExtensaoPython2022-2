3fb94c6825ca7e396e0387a2cf5a5e7012372162
Sarah Silva <81635395+SarahMaressa@users.noreply.github.com>
2022-10-20 00:32:19 +0000

    Hello World para abençoar o codigo

---
+++ main.py
@@ -0,0 +1,14 @@
+# Linha de comentario
+# Primeiro projeto Python!
+
+''' 
+
+Comentario em bloco
+
+'''
+
+# Comando de saida = print()
+# aspas simples ou duplas
+# print('Hello World!')
+print("Hello World!")
+
